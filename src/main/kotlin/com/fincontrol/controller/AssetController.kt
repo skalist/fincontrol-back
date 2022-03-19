e22bb142063f4af438f37bd446f1a5379982dd28
sergeykalistratov <skalistratov@phoenixit.ru>
2022-03-19 22:03:10 +0300

    SF-20 Implemented asset crud operations

--- src/main/kotlin/com/fincontrol/controller/AssetController.kt
+++ src/main/kotlin/com/fincontrol/controller/AssetController.kt
@@ -3,8 +3,12 @@ package com.fincontrol.controller
 import com.fincontrol.dto.AssetListDto
 import com.fincontrol.dto.AssetUpsertDto
 import com.fincontrol.service.AssetService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -19,4 +23,13 @@ class AssetController(
 
     @GetMapping("{id}")
     fun findById(@PathVariable id: UUID):AssetUpsertDto = assetService.findById(id)
+
+    @PostMapping
+    fun create(@RequestBody dto: AssetUpsertDto): UUID = assetService.create(dto)
+
+    @PutMapping
+    fun update(@RequestBody dto: AssetUpsertDto): UUID = assetService.update(dto)
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: UUID) = assetService.delete(id)
 }
--- src/main/kotlin/com/fincontrol/service/AssetService.kt
+++ src/main/kotlin/com/fincontrol/service/AssetService.kt
@@ -5,7 +5,9 @@ import com.fincontrol.dto.AssetUpsertDto
 import com.fincontrol.dto.AutocompleteOption
 import com.fincontrol.exception.EntityNotFoundException
 import com.fincontrol.model.Asset
+import com.fincontrol.model.Industry
 import com.fincontrol.repository.AssetRepository
+import com.fincontrol.repository.IndustryRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -15,6 +17,7 @@ import java.util.*
 class AssetService(
     private val assetRepository: AssetRepository,
     private val authenticationFacade: AuthenticationFacade,
+    private val industryRepository: IndustryRepository,
 ) {
     fun findAll(): List<AssetListDto> {
         val userId = authenticationFacade.getUserId()
@@ -44,4 +47,52 @@ class AssetService(
                 industry = if (it.industry != null) AutocompleteOption(it.industry.id, it.industry.name) else null
             )
         }
+
+    @Transactional
+    fun create(dto: AssetUpsertDto): UUID {
+        val userId = authenticationFacade.getUserId()
+        val industry = if (dto.industry != null) {
+            industryRepository.findById(dto.industry.value)
+                .orElseThrow { throw EntityNotFoundException(Industry::class.java.simpleName, dto.industry.value) }
+        } else {
+            null
+        }
+        val asset = Asset(
+            userId = userId,
+            name = dto.name,
+            type = dto.type,
+            currency = dto.currency,
+            code = dto.code,
+            description = dto.description,
+            industry = industry,
+        )
+
+        return assetRepository.save(asset).id
+    }
+
+    @Transactional
+    fun update(dto: AssetUpsertDto): UUID {
+        val asset = assetRepository.findById(dto.id)
+            .orElseThrow { throw EntityNotFoundException(Asset::class.java.simpleName, dto.id) }
+        val industry = if (dto.industry != null) {
+            industryRepository.findById(dto.industry.value)
+                .orElseThrow { throw EntityNotFoundException(Industry::class.java.simpleName, dto.industry.value) }
+        } else {
+            null
+        }
+
+        val copiedAsset = asset.copy(
+            name = dto.name,
+            type = dto.type,
+            currency = dto.currency,
+            code = dto.code,
+            description = dto.description,
+            industry = industry,
+        )
+
+        return assetRepository.save(copiedAsset).id
+    }
+
+    @Transactional
+    fun delete(id: UUID) = assetRepository.deleteById(id)
 }
