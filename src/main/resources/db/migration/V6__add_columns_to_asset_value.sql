ae72d7267d8072b91a0c371e1fc24b6fbb833193
sergeykalistratov <skalistratov@phoenixit.ru>
2022-03-28 22:44:31 +0300

    SF-28 Fixed asset value table

--- src/main/resources/db/migration/V6__add_columns_to_asset_value.sql
+++ src/main/resources/db/migration/V6__add_columns_to_asset_value.sql
@@ -1,2 +1,2 @@
 alter table asset_value add column broker_account_id varchar(36) references broker_account(id);
-alter table asset_value add column assetsCount integer;
+alter table asset_value add column assets_count integer;
