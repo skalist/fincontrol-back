b5e20b2c3e0686848411df78b02077138b2524a9
Sergey Kalistratov <sergey.kalistratov@gmail.com>
2022-05-15 19:01:30 +0300

    SF-83 Added controller exception handler

---
+++ src/main/kotlin/com/fincontrol/config/RestResponseEntityExceptionHandler.kt
@@ -0,0 +1,31 @@
+package com.fincontrol.config
+
+import com.fincontrol.exception.EntityNotFoundException
+import mu.KotlinLogging
+import org.hibernate.exception.ConstraintViolationException
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+    private val log = KotlinLogging.logger {  }
+
+    @ExceptionHandler(value = [EntityNotFoundException::class])
+    fun handleNotFound(ex: Exception, request: WebRequest): ResponseEntity<Any?> {
+        val message = "Entity not found"
+        log.error(ex){ message }
+        return handleExceptionInternal(ex, message, HttpHeaders(), HttpStatus.NOT_FOUND, request)
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun handleConflict(ex: Exception, request: WebRequest): ResponseEntity<Any?> {
+        val message = "Request can't be performed"
+        log.error(ex){ message }
+        return handleExceptionInternal(ex, message, HttpHeaders(), HttpStatus.CONFLICT, request)
+    }
+}
