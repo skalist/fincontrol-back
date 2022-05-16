a906b33894723e0a1e94d396b59ec8c140d81fea
Sergey Kalistratov <sergey.kalistratov@gmail.com>
2022-05-16 21:16:03 +0300

    SF-80 Commented repository and specification

--- src/main/kotlin/com/fincontrol/repository/UserRepository.kt
+++ src/main/kotlin/com/fincontrol/repository/UserRepository.kt
@@ -2,8 +2,11 @@ package com.fincontrol.repository
 
 import com.fincontrol.model.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import java.util.*
 
+/**
+ * Repository for user entity
+ */
 interface UserRepository: JpaRepository<User, UUID> {
     fun findByUsernameAndActiveIsTrue(username: String): User?
 }
--- src/main/kotlin/com/fincontrol/specification/BankOperationSpecification.kt
+++ src/main/kotlin/com/fincontrol/specification/BankOperationSpecification.kt
@@ -3,16 +3,30 @@ package com.fincontrol.specification
 import com.fincontrol.model.*
 import org.springframework.data.jpa.domain.Specification
 import java.time.LocalDate
-import java.util.UUID
+import java.util.*
 import javax.persistence.criteria.Predicate
 
+/**
+ * Specification for bank operation entity
+ */
 object BankOperationSpecification {
+    /**
+     * User id equal condition
+     * @param userId user identifier
+     * @return specification
+      */
     fun userIdEqual(userId: UUID): Specification<BankOperation> {
         return Specification<BankOperation> { root, _, cb ->
             return@Specification cb.equal(root[BankOperation_.userId], userId)
         }
     }
 
+    /**
+     * Condition for dateCreated where value between 2 dates
+     * @param startDate min value of date
+     * @param endDate max value of date
+     * @return specification
+     */
     fun createdDateBetween(startDate: LocalDate?, endDate: LocalDate?): Specification<BankOperation> {
         return Specification<BankOperation> { root, _, cb ->
             val predicates = mutableListOf<Predicate>()
@@ -27,12 +41,22 @@ object BankOperationSpecification {
         }
     }
 
+    /**
+     * Type equal condition
+     * @param type type of operation
+     * @return specification
+     */
     fun typeEqual(type: OperationType): Specification<BankOperation> {
         return Specification<BankOperation> { root, _, cb ->
             return@Specification cb.equal(root[BankOperation_.type], type)
         }
     }
 
+    /**
+     * Category id equal condition
+     * @param categoryId category identifier
+     * @return specification
+     */
     fun categoryIdEqual(categoryId: UUID?): Specification<BankOperation>? {
         if (categoryId == null) {
             return null
@@ -41,7 +65,12 @@ object BankOperationSpecification {
             return@Specification cb.equal(root[BankOperation_.operationCategory][OperationCategory_.id], categoryId)
         }
     }
-    
+
+    /**
+     * Bank account id equal condition
+     * @param bankAccountId bank account identifier
+     * @return specification
+     */
     fun bankAccountIdEqual(bankAccountId: UUID?): Specification<BankOperation>? {
         if (bankAccountId == null) {
             return null
